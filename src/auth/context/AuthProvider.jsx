2e84599c560117058bbca7b45a3881fa8a94ffa8
isshintengaku <isshintengaku@gmail.com>
2023-11-07 02:42:37 +0000

    User almacenado en localStorage

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -4,9 +4,9 @@ import { authReducer } from './authReducer';
 
 import { types } from '../types/types';
 
-const initialState = {
-  logged: false,
-};
+// const initialState = {
+//   logged: false,
+// };
 
 const init = () => {
   const user = JSON.parse(localStorage.getItem('user'));
@@ -18,16 +18,21 @@ const init = () => {
 };
 
 export const AuthProvider = ({ children }) => {
-  const [authState, dispatch] = useReducer(authReducer, initialState);
+  const [authState, dispatch] = useReducer(authReducer, {}, init);
 
   const login = (name = '') => {
+    const user = {
+      id: 'ABC',
+      name: name,
+    };
+
     const action = {
       type: types.login,
-      payload: {
-        id: 'ABC',
-        name: name,
-      },
+      payload: user,
     };
+
+    localStorage.setItem('user', JSON.stringify(user));
+
     dispatch(action);
   };
 
